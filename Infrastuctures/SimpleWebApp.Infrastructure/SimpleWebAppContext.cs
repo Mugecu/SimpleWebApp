4901baee8e49f22a343ab350ac46ab60d8291248
Mugecu <forwork_ivan@tut.by>
2023-07-01 21:06:15 +0300

    Отладка свагера и конфигурации.

--- Infrastuctures/SimpleWebApp.Infrastructure/SimpleWebAppContext.cs
+++ Infrastuctures/SimpleWebApp.Infrastructure/SimpleWebAppContext.cs
@@ -1,6 +1,23 @@
-﻿namespace SimpleWebApp.Infrastructure
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleWebApp.Domain.Entities;
+
+namespace SimpleWebApp.Infrastructure
 {
-    public class SimpleWebAppContext
+    public class SimpleWebAppContext : DbContext
     {
+        public virtual DbSet<Product> Products { get; set; }
+
+        public SimpleWebAppContext(DbContextOptions<SimpleWebAppContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasKey(i => i.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- SimpleWebApp/Program.cs
+++ SimpleWebApp/Program.cs
@@ -1,48 +1,64 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Cors.Infrastructure;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SimpleWebApp.Infrastructure;
 
 namespace SimpleWebApp
 {
     public class Program
     {
-        private const string _defaultCorsPolicyName = "AllowOrigins";
-        public IConfiguration Configuration { get; }
-
         public static void Main(string[] args)
         {
 
             var builder = WebApplication.CreateBuilder(args);
+            BuildAppSettingsProvider(builder.Configuration);
 
-            //// Add services to the container.
-            builder.Services.AddRazorPages();
+            builder.Services.AddControllers();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<SimpleWebAppContext>(options =>
+                    options.UseNpgsql(AppSettingsProvider.Configuration.GetConnectionString("SimpleWebDb")), ServiceLifetime.Scoped);
+
             builder.Services.AddCors(GetCorsOptions());
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
             {
+                using var scope = app.Services.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<SimpleWebAppContext>();
+                db.Database.EnsureCreated();
+
                 app.UseExceptionHandler("/Error");
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
             app.UseStaticFiles();
-
+            app.UseHttpsRedirection();
             app.UseRouting();
-            app.UseCors(_defaultCorsPolicyName);
+            app.UseCors(AppSettingsProvider.DefaultCorsPolicyName);
 
             app.UseAuthorization();
-
-            app.MapRazorPages();
-
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
             app.Run();
         }
 
-        private Action<CorsOptions> GetCorsOptions()
+        private static void BuildAppSettingsProvider(IConfiguration configuration)
+        {
+            AppSettingsProvider.DbConnectionString = configuration.GetConnectionString("SimpleWebDb");
+            AppSettingsProvider.DefaultCorsPolicyName = configuration.GetSection("AllowOrigins").Value;
+            AppSettingsProvider.Configuration = configuration;
+        }
+
+        private static Action<CorsOptions> GetCorsOptions()
         {
             return options =>
             {
-                options.AddPolicy(_defaultCorsPolicyName, opt =>
+                options.AddPolicy(AppSettingsProvider.DefaultCorsPolicyName, opt =>
                 {
-                    var allowed = Configuration.GetSection(_defaultCorsPolicyName)
+                    var allowed = AppSettingsProvider.Configuration.GetSection(AppSettingsProvider.DefaultCorsPolicyName)
                         .GetChildren()
                         .Select(x => x.Value)
                         .ToArray();
@@ -54,5 +70,12 @@ namespace SimpleWebApp
                 });
             };
         }
+    }
+
+    public static class AppSettingsProvider
+    {
+        public static string DbConnectionString { get; set; }
+        public static string DefaultCorsPolicyName { get; set; }
+        public static IConfiguration Configuration { get; set; }
     }
 }
