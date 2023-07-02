a073ce54ba4673ba3a9c2db4f6c6edf8bb89d5c5
Mugecu <forwork_ivan@tut.by>
2023-07-02 13:04:10 +0300

    переименование папки

---
+++ Domains/SimpleWebApp.Domain/Repositories/ProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using SimpleWebApp.Domain.Abstracts;
+using SimpleWebApp.Domain.Entities;
+
+namespace SimpleWebApp.Domain.Interfaces
+{
+    public abstract class ProductRepository : Repository<Product>
+    {
+        public abstract Task<List<KeyValuePair<Guid, string>>> ContainceProductInRepository(IEnumerable<KeyValuePair<Guid, string>> productIds);
+    }
+}
